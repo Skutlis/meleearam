ea712d00c8b3e3db6895a995a5201a8709738205
Erlend Skutlaberg <skutlis98@gmail.com>
2025-01-27 16:51:05 +0100

    Connected the database to neon

--- test.py
+++ test.py
@@ -1,5 +1,21 @@
+from db.DBManager import dbManager
+from db.data_handler import dataHandler
+
+handler= dataHandler()
+
+m1 = handler.get_champs_for_gamemode("melee")
+
+handler.ban_champ("Aatrox")
+
+m2 = handler.get_champs_for_gamemode("melee")
+
+banned = handler.get_banned_champs()
 
 
-a = "helle you there"
+print("First")
+print(m1)
+print("second")
+print(m2)
+print("Banned")
+print(banned)
 
-print(a.split(" "))
